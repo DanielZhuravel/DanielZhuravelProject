6aa07b0030fbd66273bd2fe02c2f800d0c17b2ba
DanielZhuravel <s8103@motzkine.ort.org.il>
2020-10-22 15:59:54 +0300

    v2

--- app/src/main/java/com/example/danielzhuravelproject/ActivityLogIn.java
+++ app/src/main/java/com/example/danielzhuravelproject/ActivityLogIn.java
@@ -2,7 +2,9 @@ package com.example.danielzhuravelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityLogIn extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ActivityLogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void toMainMenu(View view) {
+        Intent i=new Intent(this,ActivityMainMenu.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/danielzhuravelproject/ActivityMainMenu.java
+++ app/src/main/java/com/example/danielzhuravelproject/ActivityMainMenu.java
@@ -2,7 +2,9 @@ package com.example.danielzhuravelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityMainMenu extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class ActivityMainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void toSchedule(View view) {
+        Intent i=new Intent(this,ActivitySchedule.class);
+        startActivity(i);
+    }
+
+    public void toCalendar(View view) {
+        Intent i=new Intent(this,ActivityCalendar.class);
+        startActivity(i);
+    }
+
+    public void toSummaries(View view) {
+        Intent i=new Intent(this,ActivitySummaries.class);
+        startActivity(i);
     }
 }
