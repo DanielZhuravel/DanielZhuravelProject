2ca1677729df26636c481b66ed2df48977b1568f
DanielZhuravel <s8103@motzkine.ort.org.il>
2021-01-04 14:12:50 +0200

    UI v1

--- app/src/main/java/com/example/danielzhuravelproject/ActivityLogIn.java
+++ app/src/main/java/com/example/danielzhuravelproject/ActivityLogIn.java
@@ -5,16 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityLogIn extends AppCompatActivity {
+
+    EditText name;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        name = findViewById(R.id.etLoginName);
+        password = findViewById(R.id.etLoginPassword);
     }
 
-    public void toMainMenu(View view) {
+
+    public void toSignUp(View view) {
+        Intent i=new Intent(this,ActivitySignUp.class);
+        startActivity(i);
+    }
+
+
+    public void logIn(View view) {
+        String strName = name.getText().toString();
+        String strPassword = password.getText().toString();
+
+        Toast.makeText(this, "User name: " + strName + "\nPassword: " + strPassword, Toast.LENGTH_SHORT).show();
+
         Intent i=new Intent(this,ActivityMainMenu.class);
         startActivity(i);
     }
--- app/src/main/java/com/example/danielzhuravelproject/ActivitySignUp.java
+++ app/src/main/java/com/example/danielzhuravelproject/ActivitySignUp.java
@@ -5,17 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivitySignUp extends AppCompatActivity {
 
+    EditText name;
+    EditText email;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        name = findViewById(R.id.etSignUpName);
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.etSignUpPassword);
     }
 
 
-    public void toMainMenu(View view) {
+    public void register(View view) {
+        String strname = name.getText().toString();
+        String stremail = email.getText().toString();
+        String strpassword = password.getText().toString();
+
+        Toast.makeText(this, "User name: " + strname + "\nE-mail: " + stremail + "\nPassword: " + strpassword, Toast.LENGTH_SHORT).show();
+
         Intent i=new Intent(this,ActivityMainMenu.class);
         startActivity(i);
     }
